11f02c77cb334068e0e1614d7e5da5ab47d1f9ce
Simon Schötz <simonsch.tz@gmail.com>
2024-10-28 14:10:39 +0100

    feat: 36 set up functions to create and read tokens

---
+++ src/actions/token/index.ts
@@ -0,0 +1,2 @@
+export { generateToken } from './generate-token';
+export { readToken } from './read-token';
---
+++ src/actions/token/read-token.ts
@@ -0,0 +1,32 @@
+'use server';
+
+import { TokenPayload } from '@/types/app';
+import { assertIsString } from '@/util/asserts';
+import { parsedEnv } from '@/util/helper';
+import { jwtVerify } from 'jose';
+import { JOSEError } from 'jose/errors';
+
+export const readToken = async <T extends TokenPayload>(
+  token: string
+): Promise<T & { expiresAt: Date }> => {
+  try {
+    const authSecret = parsedEnv.TOKEN_AUTH_SECRET;
+    assertIsString(authSecret);
+
+    const secret = new TextEncoder().encode(authSecret);
+
+    const { payload } = await jwtVerify(token, secret);
+
+    const expiresAt = new Date(payload.exp! * 1000);
+
+    return { ...(payload as T), expiresAt };
+  } catch (error) {
+    if (error instanceof JOSEError) {
+      const isExpired = error.code === 'ERR_JWT_EXPIRED';
+      if (isExpired) {
+        throw new Error('Token expired');
+      }
+    }
+    throw error;
+  }
+};
