2d57e67380181512087f15a0ae914d0ef280fdd8
Simon Schötz <simonsch.tz@gmail.com>
2024-11-11 14:29:37 +0100

    production: helper functions to set cookies

---
+++ src/enums/cookie-names.ts
@@ -0,0 +1,3 @@
+export enum CookieName {
+  SESSION_SIGNATURE = 'session_signature',
+}
--- src/util/helper/index.ts
+++ src/util/helper/index.ts
@@ -1,1 +1,3 @@
 export * from './parsed-env';
+export * from './get-cookie-config';
+export * from './dates';
