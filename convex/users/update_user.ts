589eeb6c5d547a0016df6882dad9756b0e24eade
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-11-12 23:13:31 +0100

    feat: #38 update user convex func

---
+++ convex/users/update_user.ts
@@ -0,0 +1,40 @@
+import { zInternalMutation, zMutation } from '../helper';
+import { zUserDto } from '@/api/db/validators';
+import { zid } from 'convex-helpers/server/zod';
+import { DbTable, HttpStatusCode } from '@/enums';
+import { getUserByIdIntern } from './get_users';
+import { z } from 'zod';
+
+export const updateUser = zMutation({
+  args: {
+    userId: zid(DbTable.USERS),
+    userData: zUserDto.partial(),
+  },
+  output: z.object({
+    status: z.number(),
+  }),
+  handler: async (ctx, args) => {
+    return await updateUserIntern(ctx, args);
+  },
+});
+
+export const updateUserIntern = zInternalMutation({
+  args: { userId: zid(DbTable.USERS), userData: zUserDto.partial() },
+  output: z.object({
+    status: z.number(),
+  }),
+
+  handler: async (ctx, args) => {
+    const { userId, userData } = args;
+
+    const existingUser = await getUserByIdIntern(ctx, { userId });
+
+    if (!existingUser) {
+      return { status: HttpStatusCode.NOT_FOUND };
+    }
+
+    await ctx.db.patch(userId, userData);
+
+    return { status: HttpStatusCode.OK };
+  },
+});
