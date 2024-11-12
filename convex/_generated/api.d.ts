7bfd77f9a99c82516c684c6914d227d5b5a6f2a8
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-11-12 23:19:36 +0100

    feat: #38  changing to auth token secret

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -22,6 +22,11 @@ import type * as helper_index from "../helper/index.js";
 import type * as helper_zod from "../helper/zod.js";
 import type * as tables_email_addresses_table from "../tables/email_addresses_table.js";
 import type * as tables_index from "../tables/index.js";
+import type * as tables_users_table from "../tables/users_table.js";
+import type * as users_create_users from "../users/create_users.js";
+import type * as users_get_users from "../users/get_users.js";
+import type * as users_index from "../users/index.js";
+import type * as users_update_user from "../users/update_user.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -39,6 +44,11 @@ declare const fullApi: ApiFromModules<{
   "helper/zod": typeof helper_zod;
   "tables/email_addresses_table": typeof tables_email_addresses_table;
   "tables/index": typeof tables_index;
+  "tables/users_table": typeof tables_users_table;
+  "users/create_users": typeof users_create_users;
+  "users/get_users": typeof users_get_users;
+  "users/index": typeof users_index;
+  "users/update_user": typeof users_update_user;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
