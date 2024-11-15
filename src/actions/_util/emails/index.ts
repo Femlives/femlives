71ed897efeb911361a5d263c4dc419f6bf15c083
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-11-15 08:27:04 +0100

    feat: #39 server func to send verification email

---
+++ src/actions/_util/emails/index.ts
@@ -0,0 +1,1 @@
+export { sendEmailVerificationEmail } from './send-email-verification-email';
---
+++ src/actions/_util/emails/send-email-verification-email.ts
@@ -0,0 +1,48 @@
+'server-only';
+
+import { generateToken } from '../../token';
+import { resendSendEmailSignUpVerificationEmail } from '@/api/email';
+import { ServerActionResponse } from '@/types/app';
+import { HttpStatusCode } from '@/enums';
+import { getUserByEmail } from '@/actions/user/get-user';
+import { VerifyEmailTokenPayload } from '@/types/app/actions/token/payload/verify-email';
+
+export const sendEmailVerificationEmail = async (
+  userEmail: string
+): Promise<ServerActionResponse> => {
+  const user = await getUserByEmail(userEmail);
+
+  if (!user) {
+    return {
+      status: HttpStatusCode.NOT_FOUND,
+      error: { message: 'User not found' },
+    };
+  }
+
+  const { email } = user;
+
+  const payload = {
+    email,
+  } satisfies VerifyEmailTokenPayload;
+
+  const emailVerificationToken = await generateToken<VerifyEmailTokenPayload>(
+    payload,
+    'AUTH_TOKEN_SECRET'
+  );
+
+  const res = await resendSendEmailSignUpVerificationEmail(
+    emailVerificationToken,
+    userEmail
+  );
+
+  if (!res.success) {
+    return {
+      status: HttpStatusCode.BAD_REQUEST,
+      message: `Failed to send email verification email: ${res.error}`,
+    };
+  }
+
+  return {
+    status: HttpStatusCode.OK,
+  };
+};
