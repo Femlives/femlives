363eacfd52de010a1fdaa4cc66c3ba4e7704a546
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-11-15 12:57:36 +0100

    feat: #39 user action

---
+++ src/actions/user/get-user.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { dbGetUserByEmail, dbGetUserById } from '@/api/db/user/get-user';
+import { User } from '@/types/app/user';
+
+import { cache } from 'react';
+
+export const getUserById = cache(
+  async (userId: User['id']): Promise<User | null> => {
+    return await dbGetUserById(userId);
+  }
+);
+
+export const getUserByEmail = cache(
+  async (email: User['email']): Promise<User | null> => {
+    return await dbGetUserByEmail(email);
+  }
+);
