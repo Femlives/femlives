f423a85cd10c0fdd1fc59ff5306e5a69ae369270
Simon Schötz <simonsch.tz@gmail.com>
2024-11-18 14:14:31 +0100

    production: adjust pages and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+// import localFont from 'next/font/local';
 import './globals.css';
-import Footer from '@/components/Footer';
+// import Footer from '@/components/Footer';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+// const geistSans = localFont({
+//   src: './fonts/GeistVF.woff',
+//   variable: '--font-geist-sans',
+//   weight: '100 900',
+// });
+// const geistMono = localFont({
+//   src: './fonts/GeistMonoVF.woff',
+//   variable: '--font-geist-mono',
+//   weight: '100 900',
+// });
 
 export const metadata: Metadata = {
   title: 'Femlives',
   description: `Dedicated to women's health`,
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,19 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='h-full w-full'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full w-full flex flex-col justify-center items-center`}
-      >
-        <div
-          className='
-            h-full
-            flex-grow flex flex-col items-center justify-center
-            px-2
-          '
-        >
-          {children}
-        </div>
-        <Footer />
+      <body className='h-full w-full flex justify-center items-center bg-fl-primary'>
+        {children}
       </body>
     </html>
   );
