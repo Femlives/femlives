74d863f01edb0910b09feb574c672345efe66f57
Simon Schötz <simonsch.tz@gmail.com>
2024-11-21 16:31:36 +0100

    feat: convex-deployment-test remove test table

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,8 @@
 import { DbTable } from '@/enums';
-import { defineSchema, defineTable } from 'convex/server';
+import { defineSchema } from 'convex/server';
 import { emailAddressesTable, usersTable } from './tables';
 
 export default defineSchema({
   [DbTable.EMAIL_ADDRESSES]: emailAddressesTable,
   [DbTable.USERS]: usersTable,
-  test: defineTable({}),
 });
