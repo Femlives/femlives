1f2b3bad6c24c185c843fd4af457529761310065
Simon Schötz <simonsch.tz@gmail.com>
2024-11-21 12:02:21 +0100

    feat: #65 test env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import Image from 'next/image';
 import logo from '@/assets/images/fl-logo-big.png';
 
 export default function Home() {
+  // eslint-disable-next-line no-process-env
+  console.log('>>>>>>>>>>', process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
     <main className='flex flex-col items-center justify-center'>
       <Image src={logo} alt='Femlives Logo' />
