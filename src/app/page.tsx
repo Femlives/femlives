d959145378da4a0fdeb80d9589ca49af48a2aa5a
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-11-21 13:42:40 +0100

    feat: #49 cleared main page to test button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image';
-import logo from '@/assets/images/fl-logo-big.png';
+import { Button } from '@/components/Button';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center'>
-      <Image src={logo} alt='Femlives Logo' />
+      <Button buttonLabel='Login' variant='pink-outlined' />
+      <Button buttonLabel='Take test' variant='blue-filled' />
+      <Button buttonLabel='Take test' variant='pink-filled' />
     </main>
   );
 }
