cb1e88e02f5df01e82e661c3c12060014ea80140
Simon Schötz <simonsch.tz@gmail.com>
2024-11-21 12:10:44 +0100

    feat: #65 log env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,14 @@ import Image from 'next/image';
 import logo from '@/assets/images/fl-logo-big.png';
 
 export default function Home() {
-  // eslint-disable-next-line no-process-env
-  console.log('>>>>>>>>>>', process.env.NEXT_PUBLIC_CONVEX_URL);
+  console.log(
+    '>>>>>>>>>>',
+    // eslint-disable-next-line no-process-env
+    process.env.NEXT_PUBLIC_CONVEX_URL,
+    'env:',
+    // eslint-disable-next-line no-process-env
+    process.env.NODE_ENV
+  );
   return (
     <main className='flex flex-col items-center justify-center'>
       <Image src={logo} alt='Femlives Logo' />
