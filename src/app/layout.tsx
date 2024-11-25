630759bbd99c8c1d4a720cdab5a04955a0e6d3fb
Simon Schötz <simonsch.tz@gmail.com>
2024-11-25 19:56:58 +0100

    feat: #52 add font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-// import localFont from 'next/font/local';
 import './globals.css';
+import { dmSans } from './fonts';
 // import Footer from '@/components/Footer';
-
-// const geistSans = localFont({
-//   src: './fonts/GeistVF.woff',
-//   variable: '--font-geist-sans',
-//   weight: '100 900',
-// });
-// const geistMono = localFont({
-//   src: './fonts/GeistMonoVF.woff',
-//   variable: '--font-geist-mono',
-//   weight: '100 900',
-//  });
 
 export const metadata: Metadata = {
   title: 'Femlives',
@@ -25,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className='h-full w-full'>
-      <body className='h-full w-full flex justify-center items-center bg-fl-primary'>
+      <body
+        className={`h-full w-full flex flex-col justify-center items-center bg-fl-primary ${dmSans.variable} font-sans font`}
+      >
         {children}
       </body>
     </html>
