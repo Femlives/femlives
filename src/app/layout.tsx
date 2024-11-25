fc56d52b04516aa286cdb92c76ef7f842677d787
Simon Schötz <simonsch.tz@gmail.com>
2024-11-25 20:01:59 +0100

    feat: #52 some basic css utility classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,16 @@ export const metadata: Metadata = {
   title: 'Femlives',
   description: `Dedicated to women's health`,
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='h-full w-full'>
+    <html lang='en' className='xy-full'>
       <body
-        className={`h-full w-full flex flex-col justify-center items-center bg-fl-primary ${dmSans.variable} font-sans`}
+        className={`xy-full flex-center flex-col bg-fl-primary ${dmSans.variable} font-sans`}
       >
         {children}
       </body>
