adca390f30ee52d49a7319b5382eecab3b68c548
Simon Schötz <simonsch.tz@gmail.com>
2024-11-25 13:42:36 +0100

    feat: #67 set up route protection middleware

--- src/enums/routes.ts
+++ src/enums/routes.ts
@@ -2,5 +2,9 @@ export enum Route {
   GTC = '/gtc',
   HOME = '/',
   IMPRINT = '/imprint',
+  LOGIN = '/login',
+  NOT_FOUND = '/not-found',
+  PASSWORD_RESET = '/password-reset',
+  SIGN_UP = '/sign-up',
   VERIFY_EMAIL = '/verify-email',
 }
