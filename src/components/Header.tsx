e1b8c8391dc9365247d22b4c1797c71f1896fc06
Simon Schötz <simonsch.tz@gmail.com>
2024-11-28 14:14:09 +0100

    feat: #62 adjust header to show email icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import { FCProps } from '@/types/app';
 import Image from 'next/image';
-
 import logo from '../../public/images/femlives-logo.svg';
 import { Button } from './Button';
+
 const Header: FCProps = () => {
   return (
-    <header className='w-full px-36 py-4 bg-quinary flex justify-between items-center sticky top-0'>
+    <header className='w-full px-36 py-4 bg-quinary flex justify-between items-center flex-wrap sticky top-0'>
       <Image src={logo} alt='Femlives Logo' />
       <div className='flex gap-4'>
         <Button
@@ -20,6 +20,7 @@ const Header: FCProps = () => {
         />
         <Button
           buttonLabel='Subscribe'
+          iconLeft={'EmailIcon'}
           onClick={() => console.log('Subscribe clicked')}
         />
       </div>
