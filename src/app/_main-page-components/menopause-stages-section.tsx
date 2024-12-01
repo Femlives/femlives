2f961bbb60cfd9c149eba9ffde6a3555e1ea3a24
Simon Schötz <simonsch.tz@gmail.com>
2024-12-01 18:35:58 +0100

    feat: #87 finish section by rendering description

--- src/app/_main-page-components/menopause-stages-section.tsx
+++ src/app/_main-page-components/menopause-stages-section.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { H3 } from '@/components';
 import { FCProps, SetAction } from '@/types/app';
 import { useState } from 'react';
 
@@ -9,33 +10,10 @@ type MenopauseStage = {
   symptoms: string[];
 };
 
-const menopauseStages: MenopauseStage[] = [
-  {
-    title: 'Prämenopause',
-    description: 'Eine Beschreibung zur Prämenopause.',
-    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
-  },
-  {
-    title: 'Perimenopause',
-    description: 'Eine Beschreibung zur Perimenopause.',
-    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
-  },
-  {
-    title: 'Menopause',
-    description: 'Eine Beschreibung zur Menopause.',
-    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
-  },
-  {
-    title: 'Postmenopause',
-    description: 'Eine Beschreibung zur Postmenopause.',
-    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
-  },
-];
-
 export const MenopauseStagesSection: FCProps = () => {
   const [activeStage, setActiveStage] = useState<MenopauseStage | null>(null);
 
-  const getMenuItems = () => {
+  const renderMenuItems = () => {
     return menopauseStages.map((stage, index) => (
       <>
         <MenuItem
@@ -45,22 +23,21 @@ export const MenopauseStagesSection: FCProps = () => {
           isActive={activeStage?.title === stage.title}
         />
         {index !== menopauseStages.length - 1 && (
-          <div className='text-gray-light' aria-hidden>
-            |
-          </div>
+          <div className='text-gray-light w-px h-4 bg-gray-light' aria-hidden />
         )}
       </>
     ));
   };
 
   return (
-    <div className='bg-white py-10 flex-center flex-col'>
-      <div className='bg-quaternary-lighter p-6 rounded-3xl w-fit '>
+    <div className='bg-white py-10 flex-center flex-col gap-8'>
+      <div className='bg-quaternary-lighter p-6 rounded-3xl w-fit'>
         <menu className='flex items-center bg-white border border-gray-light rounded-lg p-1'>
-          {getMenuItems()}
+          {renderMenuItems()}
         </menu>
       </div>
-      {!!activeStage && <div>{activeStage.description}</div>}
+
+      {!!activeStage && <DescriptionSection stage={activeStage} />}
     </div>
   );
 };
@@ -86,3 +63,43 @@ const MenuItem = ({
     </li>
   );
 };
+
+const DescriptionSection = ({ stage }: { stage: MenopauseStage }) => {
+  return (
+    <div className='flex gap-20'>
+      <div className='w-full max-w-96'>
+        <H3 text={stage.title} className='mb-4' />
+        <p>{stage.description}</p>
+      </div>
+      <ul className='list-disc flex-center flex-col'>
+        {stage.symptoms.map((symptom) => (
+          <li key={symptom}>{symptom}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const menopauseStages: MenopauseStage[] = [
+  {
+    title: 'Prämenopause',
+    description:
+      'Eine Beschreibung zur Prämenopause. Eine Beschreibung zur Prämenopause. Eine Beschreibung zur Prämenopause. Eine Beschreibung zur Prämenopause. ',
+    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
+  },
+  {
+    title: 'Perimenopause',
+    description: 'Eine Beschreibung zur Perimenopause.',
+    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
+  },
+  {
+    title: 'Menopause',
+    description: 'Eine Beschreibung zur Menopause.',
+    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
+  },
+  {
+    title: 'Postmenopause',
+    description: 'Eine Beschreibung zur Postmenopause.',
+    symptoms: ['Symptom 1', 'Symptom 2', 'Symptom 3'],
+  },
+];
