1c518eb57d7b55df69356bf16640624c2e534bf8
Simon Schötz <simonsch.tz@gmail.com>
2024-12-01 16:25:19 +0100

    feat: #55 add hero section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-
 import Header from '@/components/Header';
+import { HeroSection } from './main-page-components';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HeroSection />
     </main>
   );
 }
