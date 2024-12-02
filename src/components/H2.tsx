83474f922e74994d4a508235683418bf51a129bd
Simon Schötz <simonsch.tz@gmail.com>
2024-12-02 12:39:58 +0100

    feat: stats-section-underlines adjust H2 to accept reference

--- src/components/H2.tsx
+++ src/components/H2.tsx
@@ -1,11 +1,14 @@
 import { FCProps, HeadingProps } from '@/types/app';
+import { forwardRef } from 'react';
 
 type Props = HeadingProps & { text: string };
 
-export const H2: FCProps<Props> = ({ text, className, ...props }) => {
-  return (
-    <h2 className={`text-4xl font-bold ${className}`} {...props}>
-      {text}
-    </h2>
-  );
-};
+export const H2: FCProps<Props> = forwardRef(
+  ({ text, className, ...props }, ref) => {
+    return (
+      <h2 className={`text-4xl font-bold ${className}`} {...props} ref={ref}>
+        {text}
+      </h2>
+    );
+  }
+);
