a95ce305b6e4a57359b43c1201213fd640df8eeb
Daria <furmandariaua@gmail.com>
2024-12-02 06:28:15 +0100

    feat: #61 faq added to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import FAQ from '@/components/FAQ';
 import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <FAQ/>
     </main>
   );
 }
